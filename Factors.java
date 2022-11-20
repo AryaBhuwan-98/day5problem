1fce7731f268a6694a6a4eb0cc800de5fa318d78
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2022-11-20 14:34:25 -0800

    Factors Problem

---
+++ Factors.java
@@ -0,0 +1,26 @@
+package com.bridgelabzday5problem;
+import java.util.Scanner;
+public class Factors {
+		public static void main(String[] args) {
+			int count = 0;
+			Scanner numbers = new Scanner(System.in);
+			System.out.println("enter any integer:");
+		    int	number = numbers.nextInt();
+		     
+		     int factor;
+				   for(factor=1;factor<=number/2;factor++) {
+					 if(number % factor == 0) {
+						System.out.println(factor+ " is a factor to the "+number);
+						count = count+1;
+					 }
+					
+					 //else{
+						//System.out.println(number+" is  a prime number");
+						//break;
+					//}
+					 
+				   }
+				   System.out.println("number of factors of "+number+" is "+count);
+	}
+	}
+
